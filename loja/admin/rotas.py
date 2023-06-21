f9eb8021fc7d421873c977e3052de768d36e2c5f
cleitone1000 <playnetnow@gmail.com>
2023-06-21 10:25:49 -0300

    update_bootstrap_css_js

---
+++ loja/admin/rotas.py
@@ -0,0 +1,15 @@
+from flask import render_template, session, request, url_for
+
+from loja import app, db
+
+
+@app.route('/')
+
+def home():
+    return "Hello World!"
+
+
+@app.route('/registrar')
+
+def registrar():
+    return render_template('admin/registrar.html', title="Registrar user")
